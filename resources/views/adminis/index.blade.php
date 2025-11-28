f14667f91db3fdc5144965feb2729c4ad8564c22
Farahbany24 <farahrabbany19@gmail.com>
2025-11-28 10:11:42 +0700

    penambahan title

--- resources/views/adminis/index.blade.php
+++ resources/views/adminis/index.blade.php
@@ -2,6 +2,7 @@
 <html>
 
 <head>
+  <title>DATA KARYAWAN</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet" />
